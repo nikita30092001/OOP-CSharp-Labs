f4a12cf3a09e8a0b1abb047ad4fff3e18b4aecd8
nikita30092001 <70797118+nikita30092001@users.noreply.github.com>
2020-12-26 18:28:17 +0300

    Add files via upload

---
+++ Lab_6/Lab_6/MathOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6
+{
+    class MathOperations
+    {
+        public static int FindMaxElement(double[] numbers)
+        {
+            var maxElementIndex = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (Math.Abs(numbers[i]) > Math.Abs(numbers[maxElementIndex]))
+                {
+                    maxElementIndex = i;
+                }
+            }
+            return maxElementIndex;
+        } 
+
+        public static double FindSum(double[] numbers, int index)
+        {
+            double sum = 0;
+            if (index == -1)
+            {
+                return 0;
+            }
+            for (int i = index + 1; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        public static int FindFirstPositiveElement(double[] numbers)
+        {
+            int index = -1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ Lab_6/Lab_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a length of array: ");
+            int length = CheckInput();
+            double[] numbers = new double[length];
+            numbers = FillArray(length);
+            OutputArray(numbers);
+            Console.WriteLine($"\r\nIndex of max element: {MathOperations.FindMaxElement(numbers)}");
+            var index = MathOperations.FindFirstPositiveElement(numbers);
+            Console.WriteLine($"Sum of elements after first positive element: {MathOperations.FindSum(numbers, index)}");
+        }
+
+        static int CheckInput()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 0)
+            {
+                Console.WriteLine("You enter invalid data!");
+            }
+            return number;
+        }
+
+        static double[] FillArray(int length)
+        {
+            double[] numbersArray = new double[length];
+            double number;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter number for array: ");
+                while (!double.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("You enter invalid data! Try again: ");
+                }
+                numbersArray[i] = number;
+            }
+            return numbersArray;
+        }
+
+        static void OutputArray(double[] array)
+        {
+            Console.Write("Your array: ");
+            foreach (double number in array)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
